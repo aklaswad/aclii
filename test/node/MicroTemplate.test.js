4a905e5ca6c204f014de2719dc9c2d02f260ecf0
Akira Sawada <aklaswad@gmail.com>
2023-01-03 11:07:45 +0900

    Add simple test

---
+++ test/node/MicroTemplate.test.js
@@ -0,0 +1,33 @@
+import render from '../../lib/MicroTemplate.js'
+import test from 'node:test'
+import assert from 'node:assert'
+import fs from 'fs'
+import YAML from 'yaml'
+import path from 'path'
+import url from 'url'
+
+
+const here = path.dirname(
+  url.fileURLToPath(import.meta.url)
+)
+const dataPath = path.resolve(here, 'data.yml')
+const dataText = fs.readFileSync(dataPath, 'utf-8')
+const data = YAML.parse(dataText)
+
+function compressSpaces (text) {
+  return text
+    .replace(/ +/gm, ' ')
+    .replace(/\n+/gm, "\n")
+    .replace(/^\s+/gm, '')
+    .replace(/\s+$/gm, '')
+}
+
+data.forEach( (testItem) => {
+  test(testItem.name, (t) => {
+    const result = render(testItem.tmpl, testItem.data)
+    assert.strictEqual(
+      compressSpaces(result),
+      compressSpaces(testItem.expect)
+    )
+  })
+})
