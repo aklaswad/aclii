a2958b4cb099af61011c7242d340bfa442dde688
Akira Sawada <aklaswad@gmail.com>
2023-01-03 14:11:02 +0900

    chore(build): Add new build

--- bin/aclii-launcher.sh
+++ bin/aclii-launcher.sh
@@ -26,50 +26,36 @@ _help () {
     page="aclii"
   fi
   case "$page" in
-  
     "aclii") cat << EOS
 Name: aclii
   A toolkit for aclii (Abstract Command Line Interface Interface)
 
 
 Commands:
-
   render | Render bash scripts generated from yaml config file. See sub commands for details.
 
 
-
 Options:
-
-  --file | Specify yaml file
-
+  --file | Specify aclii config file
   --verbose | 
-
 EOS
     ;;
-  
     "aclii.render") cat << EOS
 Name: aclii.render
   Render bash scripts generated from yaml config file. See sub commands for details.
 
 
 Commands:
-
   completion | Render and print bash auto-completion script to STDOUT.
-
 
   launcher | Render and print bash script to launch other program to STDOUT.
 
 
-
 Options:
-
-  --file | Specify yaml file
-
+  --file | Specify aclii config file
   --verbose | 
-
 EOS
     ;;
-  
     "aclii.render.completion") cat << EOS
 Name: aclii.render.completion
   Render and print bash auto-completion script to STDOUT.
@@ -77,16 +63,11 @@ Name: aclii.render.completion
 
 Commands:
 
-
 Options:
-
-  --file | Specify yaml file
-
+  --file | Specify aclii config file
   --verbose | 
-
 EOS
     ;;
-  
     "aclii.render.launcher") cat << EOS
 Name: aclii.render.launcher
   Render and print bash script to launch other program to STDOUT.
@@ -94,16 +75,11 @@ Name: aclii.render.launcher
 
 Commands:
 
-
 Options:
-
-  --file | Specify yaml file
-
+  --file | Specify aclii config file
   --verbose | 
-
 EOS
     ;;
-  
   esac
   exit 0
 }
@@ -141,62 +117,51 @@ __parse_args () {
       _aclii_debug "adding flag |$comopt|"
       _aclii_debug "Checking option $word : comopt $comopt"
       case "$comopt" in
-      
         "aclii.render--file" )
           args=$(echo $args | jq '.options["file"] = true')
           want="file"
           wanting="file"
           ;;
-      
         "aclii.render--verbose" )
           args=$(echo $args | jq '.options["verbose"] = true')
           want="string"
           wanting="verbose"
           ;;
-      
         "aclii.render.completion--file" )
           args=$(echo $args | jq '.options["file"] = true')
           want="file"
           wanting="file"
           ;;
-      
         "aclii.render.completion--verbose" )
           args=$(echo $args | jq '.options["verbose"] = true')
           want="string"
           wanting="verbose"
           ;;
-      
         "aclii.render.launcher--file" )
           args=$(echo $args | jq '.options["file"] = true')
           want="file"
           wanting="file"
           ;;
-      
         "aclii.render.launcher--verbose" )
           args=$(echo $args | jq '.options["verbose"] = true')
           want="string"
           wanting="verbose"
           ;;
-      
         * ) echo "Unknown Option $word"
           exit
           ;;
       esac
     else
       case "$cmd.$word" in
-      
         "aclii.render" )
           cmd="aclii.render"
           ;;
-      
         "aclii.render.completion" )
           cmd="aclii.render.completion"
           ;;
-      
         "aclii.render.launcher" )
           cmd="aclii.render.launcher"
           ;;
-      
         * ) echo "Unknown Command: $word"
             echo
             _help $cmd
@@ -211,31 +176,18 @@ __parse_args () {
   # Now we got the command which to be executed.
   # Handle it as it wants
   case "$cmd" in
-  
     "aclii" )
-    
       _help "$cmd"
-    
       ;;
-  
     "aclii.render" )
-    
       _help "$cmd"
-    
       ;;
-  
     "aclii.render.completion" )
-    
       _aclii_exec "$args"
-    
       ;;
-  
     "aclii.render.launcher" )
-    
       _aclii_exec "$args"
-    
       ;;
-  
     * )
       echo "Unknown command";
       _help;
