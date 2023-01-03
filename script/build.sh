808347b3ee4ff8c299cfa63dbb4db78b09566778
Akira Sawada <aklaswad@gmail.com>
2023-01-03 09:35:23 +0900

    feat(build): Do syntax check before install new build

--- script/build.sh
+++ script/build.sh
@@ -5,7 +5,13 @@ target=$(dirname $0)/../bin/aclii-launcher.sh
 cd $(dirname $0)/../build
 npm ci
 npx aclii render launcher --file ../aclii.yml > $tmp
+chmod +x $tmp
 cd -
-rm $target
-mv $tmp $target
-chmod +x $target
+if bash -c $tmp >/dev/null ; then
+  rm $target
+  mv $tmp $target
+  echo "Done build successfully"
+else
+  echo "Rendered launcher has syntax error. Abort."
+  rm $tmp
+fi
