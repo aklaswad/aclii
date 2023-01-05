9972940b33abc8583654242afc7ebc386104595b
Akira Sawada <aklaswad@gmail.com>
2023-01-05 13:14:32 +0900

    JSON is wrong. Seems $SCRIPTPATH has wrong value. print it.

--- test/bats/build.sh
+++ test/bats/build.sh
@@ -2,6 +2,8 @@
 set -ueo pipefail
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo "SCRIPT PATH |$SCRIPTPATH|"
+echo "SCRIPT PATH |$SCRIPTPATH|" 1>&2
 PARSER=$(npm exec "$SCRIPTPATH/../../bin/cli.js" $(echo '{"command":"aclii.render.parser-tester","options":{"file":"'"$SCRIPTPATH"'/test.yml"}}' | base64))
 echo "$PARSER"
 
