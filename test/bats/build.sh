d41db258e84edef4df4a86bebc78338f81a2e058
Akira Sawada <aklaswad@gmail.com>
2023-01-09 15:50:26 +0900

    test: Remove bats tests

--- test/bats/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ueo pipefail
-
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-JSON=$(printf '{"command":"aclii.render.parser-tester","options":{"file":"%s/test.yml"}}' "$SCRIPTPATH")
-BASE64=$(echo "$JSON" | base64)
-PARSER=$(npm exec "$SCRIPTPATH/../../bin/cli.js" "$BASE64")
-echo "$PARSER"
-
