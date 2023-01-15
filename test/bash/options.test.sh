d512b58de4fd1526c1c8bb49f3bfa8f8e70f14af
Akira Sawada <aklaswad@gmail.com>
2023-01-15 19:34:37 +0900

    test: Add more test around options

--- test/bash/options.test.sh
+++ test/bash/options.test.sh
@@ -20,6 +20,30 @@ eq "can get value from first option" \
 eq "can get value from second option" \
   "$(_q --pos 1 multi1)" "bar"
 
+__arg_parse cmdD foo
+  should_success "cmdD can take arg"
+eq "it's foo" $(_q cmdDarg) "foo"
 
+__arg_parse cmdD --cmdDopt bar
+  should_success "cmdD can also take option"
+eq "it's foo" $(_q cmdDopt) "bar"
+
+__arg_parse cmdD --cmdDopt fizz buzz
+  should_success "cmdD can take both option and arg"
+eq "arg is buzz" $(_q cmdDarg) "buzz"
+eq "opt is fizz" $(_q cmdDopt) "fizz"
+
+__arg_parse cmdD hey --cmdDopt ho
+  should_success " (?) can put option after arg if arg is not many type"
+eq "got opt val" "$(_q cmdDopt)" "ho"
+eq "got opt val" "$(_q cmdDarg)" "hey"
+
+__arg_parse cmdD --cmdDopt
+  should_success "Also, this should fail since option value is missing"
+eq "got empty opt val" "$(_q cmdDopt)" ""
+
+__arg_parse cmdD -- --cmdDopt
+  should_success "option terminator works"
+eq "arg can take string looks like option" $(_q cmdDarg) "--cmdDopt"
 
 done_testing
